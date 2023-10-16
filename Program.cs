47cb56e1f33142eef5ef097df3543039b575c658
MosutLarisa <larisa.mosut@emanuel.ro>
2023-10-16 09:48:08 +0300

    boxing

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace boxing_and_unboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //boxing
+            int i = 3;
+            object obj1 = i;
+
+
+            Console.WriteLine(i);
+            Console.WriteLine(obj1);
+
+
+            //unboxing
+            object obj2 = 4;
+            int j = (int)obj2;
+            Console.WriteLine(obj2);
+            Console.WriteLine(j);
+        }
+        
+    }
+
+    public class Person
+    {
+        public string Name;
+    }
+}
